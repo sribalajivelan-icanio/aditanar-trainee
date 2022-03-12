67160a97515cd825652c552192e81087ff8032eb
sribalajivelan-icanio <sribalajivelan.s@icanio.com>
2022-03-12 16:53:16 +0530

    Added cross-origin

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -6,6 +6,7 @@ import com.example.demo.repository.Mongorepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequestMapping("api/v1/")
 @RestController
 public class MyController {
